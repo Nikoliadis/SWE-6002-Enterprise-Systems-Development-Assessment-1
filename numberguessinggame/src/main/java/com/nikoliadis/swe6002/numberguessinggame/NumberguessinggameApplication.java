b7647a7d954088cfd10aeb8ddf18d8174297dfc8
Nikoliadis <nnikoliadis@nyc.gr>
2025-12-01 17:36:20 +0200

    Added the spring

---
+++ numberguessinggame/src/main/java/com/nikoliadis/swe6002/numberguessinggame/NumberguessinggameApplication.java
@@ -0,0 +1,13 @@
+package com.nikoliadis.swe6002.numberguessinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberguessinggameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberguessinggameApplication.class, args);
+	}
+
+}
---
+++ numberguessinggame/src/test/java/com/nikoliadis/swe6002/numberguessinggame/NumberguessinggameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikoliadis.swe6002.numberguessinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberguessinggameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
